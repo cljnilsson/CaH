6b03b4f6728ee968975e9f1de60898fb6331cb70
cljnilsson <ln222pd@student.lnu.se>
2020-03-17 02:12:14 +0100

    Panic upload for production because I used localhost as a raw input.

--- client/js/Libs/io.js
+++ client/js/Libs/io.js
@@ -1,4 +1,4 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:3001");
+const socket = openSocket("/");
 
 export default socket;
--- public/index_bundle.js
+++ public/index_bundle.js
@@ -3409,7 +3409,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var socket_io_client__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/lib/index.js");
 /* harmony import */ var socket_io_client__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(socket_io_client__WEBPACK_IMPORTED_MODULE_0__);
 
-var socket = socket_io_client__WEBPACK_IMPORTED_MODULE_0___default()("http://localhost:3001");
+var socket = socket_io_client__WEBPACK_IMPORTED_MODULE_0___default()("/");
 /* harmony default export */ __webpack_exports__["default"] = (socket);
 
 /***/ }),
